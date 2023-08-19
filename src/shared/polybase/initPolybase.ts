6eff95551b2f896b34dc342967e208fbf72c2912
khalifa47 <khalifafumo5@gmail.com>
2023-08-19 11:44:28 +0300

    initi polybase

--- src/shared/polybase/initPolybase.ts
+++ src/shared/polybase/initPolybase.ts
@@ -1,5 +1,6 @@
 import { Polybase } from '@polybase/client';
 
-const db = new Polybase({
-  defaultNamespace: 'your-namespace',
+export const db = new Polybase({
+  defaultNamespace:
+    'pk/0xc225ac31e72d45928e8ac1f5a233427ecf0f627c228d1cf043c099c08714a71b986120f0a77c6f3a543ebc5f6263aaef67d38b728f25ba14aa8e6bb3b632bd36/khalifa',
 });
