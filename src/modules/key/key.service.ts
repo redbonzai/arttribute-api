9ebca25e8e168cfe5cc54f2762f931fb1904fea4
Gitau Gakwa <ggakwa.dev@gmail.com>
2023-09-02 23:14:14 +0300

    Added different generator

--- src/modules/key/key.service.ts
+++ src/modules/key/key.service.ts
@@ -6,6 +6,7 @@ import { PolybaseService } from '~/shared/polybase';
 import { v4 as uuidv4 } from 'uuid';
 import * as bycrypt from 'bcrypt';
 import * as nanoid from 'nanoid';
+import { sha256 } from 'sha.js';
 
 @Injectable()
 export class KeyService {
@@ -15,6 +16,15 @@ export class KeyService {
   constructor(private polybaseService: PolybaseService) {
     this.db = polybaseService.app('bashy');
     this.collection = this.db.collection('ApiKey');
+  }
+
+  public async generate(props: {}) {
+    // UUID to hex
+    const buffer = Buffer.alloc(16);
+    uuidv4({}, buffer);
+    const key = buffer.toString('base64');
+
+    const hashed = this.hash(key);
   }
 
   async createKey(keyDto: KeyDto, userId: string) {
@@ -42,5 +52,8 @@ export class KeyService {
     ]);
     return { data: key.data, apikey: `${keyprefix}.${value}` };
   }
-}
 
+  private hash(val: string) {
+    return new sha256().update(val).digest('base64');
+  }
+}
